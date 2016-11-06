83bf843878964ef49d266ce060bfbc64cdbb40f2
Polankiewicz <damianpol2@o2.pl>
2016-11-06 17:42:46 +0100

    direktors drawing

--- TSK_Project/Assets/Scripts/YagiUdaDrawing.cs
+++ TSK_Project/Assets/Scripts/YagiUdaDrawing.cs
@@ -4,21 +4,35 @@ using System.Collections;
 public class YagiUdaDrawing : MonoBehaviour {
 
     private GameObject DipolElement;
-    public Vector3 vector1;
-    public Vector3 vector2;
+    private Vector3 DirektorPosition;
+    private GameObject DirektorElement;
+    
+    private Vector3 vector1;
+    private Vector3 vector2;
+
+    private int numberOfDirektors;
 
     // Use this for initialization
     void Start () {
         DipolElement = GameObject.Find("DipolElement");
-        vector1 = new Vector3(0, 0, 0.1F);
-        vector2 = new Vector3(0, 0, 0.05F);
+        DirektorElement = GameObject.Find("DirektorElement");
+        vector1 = new Vector3(0, 0, 0.4F);
+        vector2 = new Vector3(0, 0, 0.2F);
+
+        numberOfDirektors = 0;
+        DirektorPosition = DirektorElement.transform.position;
     }
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKey("z")) {
+        if (Input.GetKeyDown("z")) {
+            //resize and move dipol
             DipolElement.transform.localScale += vector1;
             DipolElement.transform.Translate(vector2);
+
+            //new direktor
+            numberOfDirektors++;
+            Instantiate(DirektorElement, DirektorPosition + new Vector3(0, 0, 0.5F * numberOfDirektors), Quaternion.identity);
         }
     }
 }
