266c4bdcb9d42650eb23a7dc64251251583ae2ba
Polankiewicz <damianpol2@o2.pl>
2016-11-07 21:39:20 +0100

    swiching scenes

---
+++ TSK_Project/Assets/Scripts/SwichingScenes.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwichingScenes : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("1"))
+            Application.LoadLevel("Yagi_Uda");
+        if (Input.GetKeyDown("2"))
+            Application.LoadLevel("Monopol");
+        if (Input.GetKeyDown("3"))
+            Application.LoadLevel("Dipol");
+        if (Input.GetKeyDown("4"))
+            Application.LoadLevel("Log_Periodic");
+    }
+}
