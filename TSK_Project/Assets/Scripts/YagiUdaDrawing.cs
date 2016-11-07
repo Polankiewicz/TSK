2481e26de063018d1aa82290caf1c4214deb99b5
Polankiewicz <damianpol2@o2.pl>
2016-11-07 20:35:05 +0100

    drawing and removing direktors

--- TSK_Project/Assets/Scripts/YagiUdaDrawing.cs
+++ TSK_Project/Assets/Scripts/YagiUdaDrawing.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class YagiUdaDrawing : MonoBehaviour {
 
@@ -11,28 +12,52 @@ public class YagiUdaDrawing : MonoBehaviour {
     private Vector3 vector2;
 
     private int numberOfDirektors;
+    private int maxNumberOfDirektors;
+
+    private Stack<Object> DirektorsStack;
+
 
     // Use this for initialization
     void Start () {
         DipolElement = GameObject.Find("DipolElement");
         DirektorElement = GameObject.Find("DirektorElement");
-        vector1 = new Vector3(0, 0, 0.4F);
-        vector2 = new Vector3(0, 0, 0.2F);
+        vector1 = new Vector3(0, 0, 0.48F);
+        vector2 = new Vector3(0, 0, 0.24F);
 
         numberOfDirektors = 0;
+        maxNumberOfDirektors = 9;
         DirektorPosition = DirektorElement.transform.position;
+
+        DirektorsStack = new Stack<Object>();
     }
 	
 	// Update is called once per frame
 	void Update () {
         if (Input.GetKeyDown("z")) {
-            //resize and move dipol
-            DipolElement.transform.localScale += vector1;
-            DipolElement.transform.Translate(vector2);
+            if(numberOfDirektors < maxNumberOfDirektors)  {
+                //resize and move dipol
+                DipolElement.transform.localScale += vector1;
+                DipolElement.transform.Translate(vector2);
 
-            //new direktor
-            numberOfDirektors++;
-            Instantiate(DirektorElement, DirektorPosition + new Vector3(0, 0, 0.5F * numberOfDirektors), Quaternion.identity);
+                //new direktor
+                numberOfDirektors++;
+                Object newDirektor = Instantiate(DirektorElement, DirektorPosition + new Vector3(0, 0, 0.5F * numberOfDirektors), Quaternion.identity);
+                DirektorsStack.Push(newDirektor);
+            }
+        }
+
+        if (Input.GetKeyDown("x"))
+        {
+            if (numberOfDirektors > 0)
+            {
+                //resize and move dipol
+                DipolElement.transform.localScale -= vector1;
+                DipolElement.transform.Translate(-vector2);
+
+                //remove direktor
+                numberOfDirektors--;
+                Destroy(DirektorsStack.Pop());
+            }
         }
     }
 }
